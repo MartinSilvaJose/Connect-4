239d6d01a752b9d5939f760cbe6dde919ca1a34b
MartinSilvaJose <118887247+MartinSilvaJose@users.noreply.github.com>
2024-03-15 20:42:02 +0100

    Game logic finalized

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,88 @@
 import { useState } from 'react'
 import './App.css'
-
-const PLAYER = {
-  RED: 'redCoin',
-  YELLOW: 'yellowCoin'
-}
+import { PLAYER, ROWS, POSITIONS } from './constants';
+import confetti from 'canvas-confetti';
 
 export function App() {
+
   const [board, setBoard] = useState(Array(7).fill(Array(7).fill(null)));
 
   const [player, setPlayer] = useState(PLAYER.RED);
+
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (index, player, board) => {
+    const [row, column] = index;
+    for (const [nextRow, nextColumn] of POSITIONS) {
+      let contador = 1; // Incluye la ficha que acaba de ser colocada
   
+      // Verificar en una dirección
+      for (let i = 1; i < 4; i++) {
+        const newRow = row + (nextRow * i);
+        const newColumn = column + (nextColumn * i);
+        if (newRow < 0 || newRow >= 7 || newColumn < 0 || newColumn >= 7 || board[newRow][newColumn] !== player) break;
+        contador++;
+      }
+  
+      // Verificar en la dirección opuesta
+      for (let i = 1; i < 4; i++) {
+        const newRow = row - nextRow * i;
+        const newColumn = column - nextColumn * i;
+        if (newRow < 0 || newRow >= 7 || newColumn < 0 || newColumn >= 7 || board[newRow][newColumn] !== player) break;
+        contador++;
+      }
+  
+      if (contador >= 4) return true;
+    }
+  
+    return null;
+  }
+  // const checkEndGame = (newBoard) => {
+  //   //Revisamos si el tablero esta lleno
+  //   return newBoard.every((square) => square !== null);
+  // }
+
+  const gravitity = (index, board) => {
+    const [y, x] = index;
+    let place = 0;
+    do {
+      if (board[place][x] !== null) {
+        place++
+      }else{
+        return [place, x];
+      }
+    }while(place < 7);
+  }
 
   const updateBoard = (index) => {
-    const [y, x] = index;
+    // const [y, x] = index;
+    const [y, x] = gravitity(index, board);
     //If there is something it does nothing
-    if(board[y][x]) return 
+    if (board[y][x] || winner) return
     //Copia profunda
     const newBoard = Array.from(JSON.parse(JSON.stringify(board)));
     //We look for the index of the square we want to mark and mark it
     newBoard[y][x] = player;
     setBoard(newBoard);
+
+    const newWinner = checkWinner([y, x], player, newBoard);
+    console.log(newWinner);
+    if (newWinner) {
+      confetti();
+      setWinner(newWinner);
+      // } else if(checkEndGame(newBoard)){
+      //   setWinner(false);
+    }
     //Change player
     const newPlayer = player === PLAYER.RED ? PLAYER.YELLOW : PLAYER.RED;
     setPlayer(newPlayer);
 
   }
-  const resetGame = () => console.log('jaja saludos')
+  const resetGame = () => {
+    setBoard(Array(7).fill(Array(7).fill(null)));
+    setPlayer(PLAYER.RED);
+    setWinner(null);
+  }
   return (
     <>
       <h1>Connect 4</h1>
@@ -43,7 +99,7 @@ export function App() {
                   return (
                     <Square
                       key={i + j}
-                      index={[i,j]}
+                      index={[i, j]}
                       updateBoard={updateBoard}
                     >
                       {cell}
@@ -51,41 +107,55 @@ export function App() {
                   )
                 })
               )
-              
+
             })
           }
+          {/* Ganador */}
+          {
+            winner !== null && (
+              <section className="winner">
+                <div className='text'>
+                  <h2>
+                    {winner === false ? 'Empate' : 'Ganó'}
+                  </h2>
+
+                  <header className='win'>
+                    {winner && (
+                      <div className={`coin ${player !== PLAYER.RED ? PLAYER.RED : PLAYER.YELLOW}`}></div>
+                    )}
+                  </header>
+
+                  <footer>
+                    <button onClick={resetGame}>Empezar de nuevo</button>
+                  </footer>
+                </div>
+              </section>
+            )
+          }
         </main>
-    </section>
+      </section>
 
 
     </>
   )
 }
 
-const ROWS = {
-  0: '7',
-  1: '6',
-  2: '5',
-  3: '4',
-  4: '3',
-  5: '2',
-  6: '1',
-
-}
-
 // eslint-disable-next-line react/prop-types
-export function Square({children, index, updateBoard}) {
-  const handleClick = () =>{
-    updateBoard(index)
+export function Square({ children, index, updateBoard }) {
+
+  const [row, column] = index;
+
+  const handleClick = () => {
+    updateBoard(index);
   }
-  const [row,column] = index;
+  
   return (
     <div
       className={`square ${children ?? ''}`}
       onClick={handleClick}
-      style={{gridRow: ROWS[row], gridColumn: column + 1}}
+      style={{ gridRow: ROWS[row], gridColumn: column + 1 }}
     >
-      {row +' '+ column}
+      {row + ', ' + column}
     </div>
   )
 
---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const PLAYER = {
+    RED: 'redCoin',
+    YELLOW: 'yellowCoin'
+}
+
+export const ROWS = {
+    0: '7',
+    1: '6',
+    2: '5',
+    3: '4',
+    4: '3',
+    5: '2',
+    6: '1',
+}
+
+export const POSITIONS = [
+    [1, -1], [1, 0], [1,1],
+    [0, -1], [0, 1],
+    [-1, -1], [-1, 0], [-1, 1]
+]
